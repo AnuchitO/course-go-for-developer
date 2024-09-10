5c313c232127b12054cfece7284f940308999508
Nong AnuchitO <anuchito.code@gmail.com>
2024-09-10 16:22:18 +0700

    flight design endpoint

--- server.go
+++ server.go
@@ -98,8 +98,15 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", handler)
-	r.GET("/flights", GetAllFlightsHandler)
-	r.GET("/flights/:id", GetFlightByIDHandler)
-	r.POST("/flights", CreateFlightHandler)
+	r.GET("/flights", GetAllFlightsHandler)     // SELECT * FROM flights;
+	r.GET("/flights/:id", GetFlightByIDHandler) // SELECT * FROM flights WHERE id = ?;
+	r.POST("/flights", CreateFlightHandler)     // INSERT INTO flights (id, number, airlineCode, destination, arrival) VALUES (?, ?, ?, ?, ?);
+	// POST /flights
+	// POST /flights/:id
+
+	// POST /flights/actions/create
+	// POST /flights/actions/update
+	// POST /flights/actions/delete
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
