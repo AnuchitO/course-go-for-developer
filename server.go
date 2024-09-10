42c121907bb49302e1edd0b8228c93b42e44fed6
Nong AnuchitO <anuchito.code@gmail.com>
2024-09-10 14:04:20 +0700

    server.go

--- server.go
+++ server.go
@@ -13,6 +13,8 @@ func flightHandler(w http.ResponseWriter, r *http.Request) {
 			"name": "anuchitO",
 			"age": 25
 			}`
+
+		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(raw))
 		return
 	}
@@ -20,8 +22,16 @@ func flightHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+func MyHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	http.HandleFunc("/flights", flightHandler)
+
+	h := http.HandlerFunc(flightHandler)
+
+	http.Handle("/anuchito", h)
 
 	log.Println("start server at port :8080")
 	err := http.ListenAndServe(":8080", nil)
